61eb21f99846c992fd25ac04e25090360f34e063
Julio Apaza <juliovladimiraz@gmail.com>
2024-07-16 20:23:46 -0500

    Just added a few bugs

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a Sarrus Rule matrix determinant calculator programmed in C++.
 
-You can enter the dimension of the square matrix and the values of its cells.
-Once the shade is created and filled, the algorithm will proceed to calculate the determinant, returning its value and the number of mathematical operations performed.
+You can enter the values of the cells of the matrix.
+Once the matrix is filled, the algorithm will proceed to calculate the determinant, returning its value and the number of mathematical operations performed.
 
 This is a personal project created with the purpose of practicing the C++ programming language.
--- SarrusDeterminantCalculator.cpp
+++ SarrusDeterminantCalculator.cpp
@@ -15,23 +15,58 @@ void printMatrix(int matrix[3][3]){
 	}
 }
 
-void calculateDeterminant(int matrix[3][3]){
-	int product = 0, i = 0, j = 0;
-	product = matrix[i][j];
-    for (i = 0; i < 3; i++){
-		product *= matrix[i][j];
+void printExtendedMatrix(int matrix[3][5]){
+    for (int i = 0; i < 3; i++){
+		for (int j = 0; j < 5; j++) {
+			if (matrix[i][j] < 10) {
+				cout << "0" << matrix[i][j] << " ";
+			} else { cout << matrix[i][j] << " "; }
+		}
+		cout << endl;
+	}
+}
+
+// function to increase the number of columns of the matrix to be able to apply the Sarrus algorithm
+int extendMatrix(int matrix[3][3]){
+	int extendedMatrix[3][5];
+	for (int i = 0; i < 3; i++){
+		for (int j = 0; j < 3; j++) {
+			extendedMatrix[i][j] = matrix[i][j];
+		}
+	}
+	extendedMatrix[0][3] = matrix[0][0];
+	extendedMatrix[1][3] = matrix[1][0];
+	extendedMatrix[2][3] = matrix[2][0];
+	extendedMatrix[0][4] = matrix[0][1];
+	extendedMatrix[1][4] = matrix[1][1];
+	extendedMatrix[2][4] = matrix[2][1];
+	printExtendedMatrix(extendedMatrix);
+	return extendedMatrix[3][5];
+}
+
+void sarrusAlgorithm(int matrix[3][5]){
+	int positiveDiagonalProducts = 1;
+	int j = 0;
+    for (int i = 0; i < 3; i++){
+		positiveDiagonalProducts *= matrix[i][j];
 		j++;
 	}
-	cout << product;
+	cout << endl << positiveDiagonalProducts << endl;
 }
 
 int main(){
     int matrix[3][3] = {{1, 2, 3},
                         {4, 5, 6},
                         {7, 8, 9}};
+	int a = 0, b = 0;
+	//cout << matrix[0][2]; == 3
+	//cout << matrix[2][0]; == 7
     
 	printMatrix(matrix);
-	calculateDeterminant(matrix);
+	cout << endl;
+	//extendMatrix(matrix);
+	int ExtendedMatrix[3][5] {extendMatrix(matrix)};
+	sarrusAlgorithm(ExtendedMatrix);
 
     return 0;
 }
