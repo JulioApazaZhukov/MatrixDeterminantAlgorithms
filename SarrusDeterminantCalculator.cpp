89141075749c5c4044835aa988fc50fe3cb0481f
Julio Apaza <juliovladimiraz@gmail.com>
2024-07-20 13:12:13 -0500

    Working calculator

--- SarrusDeterminantCalculator.cpp
+++ SarrusDeterminantCalculator.cpp
@@ -1,32 +1,9 @@
 #include <iostream>
 
-//using std::cin;
 using std::cout;
 using std::endl;
 
-void printMatrix(int matrix[3][3]){
-    for (int i = 0; i < 3; i++){
-		for (int j = 0; j < 3; j++) {
-			if (matrix[i][j] < 10) {
-				cout << "0" << matrix[i][j] << " ";
-			} else { cout << matrix[i][j] << " "; }
-		}
-		cout << endl;
-	}
-}
-
-void printExtendedMatrix(int matrix[3][5]){
-    for (int i = 0; i < 3; i++){
-		for (int j = 0; j < 5; j++) {
-			if (matrix[i][j] < 10) {
-				cout << "0" << matrix[i][j] << " ";
-			} else { cout << matrix[i][j] << " "; }
-		}
-		cout << endl;
-	}
-}
-
-// function to increase the number of columns of the matrix to be able to apply the Sarrus algorithm
+// Function to increase the number of columns of the matrix to be able to apply the Sarrus algorithm
 int extendMatrix(int matrix[3][3], int I, int J){
 	int extendedMatrix[3][5];
 	for (int i = 0; i < 3; i++){
@@ -40,40 +17,56 @@ int extendMatrix(int matrix[3][3], int I, int J){
 	extendedMatrix[0][4] = matrix[0][1];
 	extendedMatrix[1][4] = matrix[1][1];
 	extendedMatrix[2][4] = matrix[2][1];
-	//printExtendedMatrix(extendedMatrix);
-	/*for (int i = 0; i < 3; i++){
-		for (int j = 0; j < 5; j++) {
-			return extendedMatrix[i][j];
-		}
-	}*/
 	return extendedMatrix[I][J];
 }
 
+// Function to print the 3 x 3 matrix on the console
+void printMatrix(int matrix[3][3]){
+    for (int i = 0; i < 3; i++){
+		for (int j = 0; j < 3; j++) {
+			if (matrix[i][j] < 10) {
+				cout << "0" << matrix[i][j] << " ";
+			} else { cout << matrix[i][j] << " "; }
+		}
+		cout << endl;
+	}
+}
+
+// Function to print the 3 x 5 matrix on the console
+void printExtendedMatrix(int matrix[3][5]){
+    for (int i = 0; i < 3; i++){
+		for (int j = 0; j < 5; j++) {
+			if (matrix[i][j] < 10) {
+				cout << "0" << matrix[i][j] << " ";
+			} else { cout << matrix[i][j] << " "; }
+		}
+		cout << endl;
+	}
+}
+
+// Function to calculate the determinant by Sarrus algorithm
 int sarrusAlgorithm(int matrix[3][5]){
 	int determinant = 0;
 	for (int n = 0; n < 3; n++){
 		int i = 0;
 		int j = n;
 		int positiveDiagonalProduct = 1;
-		//cout << endl << positiveDiagonalProduct << endl;
 		cout << i << " " << j << " | ";
 		for (i = 0; i < 3; i++){
 			positiveDiagonalProduct = positiveDiagonalProduct * matrix[i][j];
 			cout << i << " " << j << " | ";
 			j++;
 		}
-		cout << endl << positiveDiagonalProduct << endl;
-		cout << endl;
+		cout << endl << positiveDiagonalProduct << endl;	
 		determinant += positiveDiagonalProduct;
 	}
+	cout << endl << endl;
 	for (int n = 0; n < 3; n++){
 		int i = 0;
 		int j = 2 + n;
 		int negativeDiagonalProduct = 1;
-		//cout << endl << negativeDiagonalProduct << endl;
 		cout << i << " " << j << " | ";
 		for (i = 0; i < 3; i++){
-			//i = 0;
 			cout << i << " " << j << " | ";
 			negativeDiagonalProduct = negativeDiagonalProduct * matrix[i][j];
 			j--;
@@ -81,17 +74,20 @@ int sarrusAlgorithm(int matrix[3][5]){
 		cout << endl << negativeDiagonalProduct << endl;
 		determinant -= negativeDiagonalProduct;
 	}
+	cout << endl;
 	return determinant;
 }
 
 int main(){
-    int matrix[3][3] = {{1, 2, 3},
-                        {4, 5, 6},
-                        {7, 8, 9}};
-	int a = 0, b = 0;
-	//cout << matrix[0][2]; == 3
-	//cout << matrix[2][0]; == 7
+    int matrix[3][3];
+	for (int i = 0; i < 3; i++){
+		for (int j = 0; j < 3; j++){
+			cout << "Enter: " << i << " " << j << ":	";
+			std::cin >> matrix[i][j];
+		}
+	}
     
+	cout << endl;
 	printMatrix(matrix);
 	cout << endl;
 
@@ -103,8 +99,9 @@ int main(){
 	}
 
 	printExtendedMatrix(ExtendedMatrix);
-	//cout << endl << ExtendedMatrix[0][0] << endl;
-	cout << sarrusAlgorithm(ExtendedMatrix);
+	cout << "The determinant is: " << sarrusAlgorithm(ExtendedMatrix);
 
+	int exit;
+	std::cin >> exit;
     return 0;
 }
